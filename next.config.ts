ab5b1a734a9bcf21256e5abb7f0e919405c1b107
beatrisiis <isisbeatrizsp2010@hotmail.com>
2025-05-03 08:20:31 -0300

    setting out

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/juliacomunica', // substitua pelo nome do seu repositório
+  images: {
+    unoptimized: true, // necessário para exportação
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
